d0cd8e634a6dcc0ccb90aa691dc8d4927d1383ab
Isaac Hudson <ice1080@gmail.com>
2022-09-06 22:41:03 -0700

    start of spotify service

--- src/main/java/com/ice1080/spotifyreleases/controllers/MainController.java
+++ src/main/java/com/ice1080/spotifyreleases/controllers/MainController.java
@@ -1,13 +1,19 @@
 package com.ice1080.spotifyreleases.controllers;
 
+import com.ice1080.spotifyreleases.services.SpotifyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    SpotifyService spotifyService;
+
     @GetMapping("spotify-redirect")
     void spotifyRedirect() {
         // todo
     }
+
 }
