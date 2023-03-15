4137cf12659c671d87326be1255be2739b21c718
Isaac Hudson <ice1080@gmail.com>
2023-03-14 19:11:47 -0700

    adding initial useSpotify hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
 import logo from "./spotifyLogo.svg";
 import "./App.css";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotifyApi = new SpotifyWebApi();
 
 function App() {
+  useEffect(() => {
+    spotifyApi.getArtistAlbums("43ZHCT0cAZBISjO8DG9PnE").then(
+      (data) => {
+        console.log("data", data);
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { SpotifyProvider } from "./hooks/useSpotify";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <RouterProvider router={router}>
+      <SpotifyProvider />
+    </RouterProvider>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
